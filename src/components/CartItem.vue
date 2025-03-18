c9e52939dc23174ff65248d7b7c459a925c825c8
Heavenly-venerable <ricrichar94@gmail.com>
2025-03-18 19:35:08 +0700

    feat(ui): add daisyui & add some component

---
+++ src/components/CartItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Product } from "../types/cart.ts";
+
+const props = defineProps<{ item: Product }>();
+</script>
+
+<template>
+  <div>
+    <div class="card card-compact bg-slate-800 shadow-md">
+      <figure>
+        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{{ props.item.title }}</h2>
+        <p>{{ props.item.description }}</p>
+        <p>{{ props.item.price }}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
